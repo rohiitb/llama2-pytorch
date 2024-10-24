57f1f4552208e2f5a27983f67f5a9197e52cc733
Rohit Bhikule <bhikulerohit@gmail.com>
2024-10-24 14:37:13 -0400

    add config file

---
+++ config.py
@@ -0,0 +1,10 @@
+model_config = {
+    "dim": 4096,
+    "n_layers": 32,
+    "n_heads": 32,
+    "n_kv_heads": None,
+    "vocab_size": -1,
+    "norm_eps": 1e-5,
+    "max_batch_size_kv": 32,
+    "max_seq_length_kv": 2048,
+}
